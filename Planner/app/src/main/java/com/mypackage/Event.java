5287ae03c1c973d1a842232da55a51f62546a2a8
Zzzsycide <2133553608@qq.com>
2025-04-15 17:02:18 +0800

    Update Event.java

--- Planner/app/src/main/java/com/mypackage/Event.java
+++ Planner/app/src/main/java/com/mypackage/Event.java
@@ -1,1 +1,34 @@
+package com.mypackage;
+
+public class Event {
+    private String title;
+    private String description;
+    private String location;
+
+    public Event() {}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
 
