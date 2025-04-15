d338a2041633434f778890e6a5e3081d8f05bc87
Zzzsycide <2133553608@qq.com>
2025-04-15 16:59:56 +0800

    Update MainActivity.java

--- Planner/app/src/main/java/com/mypackage/MainActivity.java
+++ Planner/app/src/main/java/com/mypackage/MainActivity.java
@@ -1,1 +1,33 @@
+package com.mypackage;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private EventAdapter eventAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        eventAdapter = new EventAdapter(this);
+        recyclerView.setAdapter(eventAdapter);
+
+        findViewById(R.id.btnAddEvent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddEventActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
 
