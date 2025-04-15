1aad444b4eac3ceb868ffda5a9b63b959bb5dbe8
Zzzsycide <2133553608@qq.com>
2025-04-15 17:01:13 +0800

    Update EventAdapter.java

--- Planner/app/src/main/java/com/mypackage/EventAdapter.java
+++ Planner/app/src/main/java/com/mypackage/EventAdapter.java
@@ -1,1 +1,52 @@
+package com.mypackage;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHolder> {
+    private Context context;
+    private List<Event> events;
+
+    public EventAdapter(Context context) {
+        this.context = context;
+        this.events = DatabaseHelper.getAllEvents(context);
+    }
+
+    @NonNull
+    @Override
+    public EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_event, parent, false);
+        return new EventViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EventViewHolder holder, int position) {
+        Event event = events.get(position);
+        holder.tvTitle.setText(event.getTitle());
+        holder.tvDescription.setText(event.getDescription());
+        holder.tvLocation.setText(event.getLocation());
+    }
+
+    @Override
+    public int getItemCount() {
+        return events.size();
+    }
+
+    public static class EventViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTitle, tvDescription, tvLocation;
+
+        public EventViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvDescription = itemView.findViewById(R.id.tvDescription);
+            tvLocation = itemView.findViewById(R.id.tvLocation);
+        }
+    }
+}
 
