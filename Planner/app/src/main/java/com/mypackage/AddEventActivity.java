53ab172af0dbb610e9aa8de2006732496f0dfb7c
Zzzsycide <2133553608@qq.com>
2025-04-15 17:00:37 +0800

    Update AddEventActivity.java

--- Planner/app/src/main/java/com/mypackage/AddEventActivity.java
+++ Planner/app/src/main/java/com/mypackage/AddEventActivity.java
@@ -1,1 +1,45 @@
+package com.mypackage;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddEventActivity extends AppCompatActivity {
+    private EditText etTitle, etDescription, etLocation;
+    private Button btnAdd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+
+        etTitle = findViewById(R.id.etTitle);
+        etDescription = findViewById(R.id.etDescription);
+        etLocation = findViewById(R.id.etLocation);
+        btnAdd = findViewById(R.id.btnAdd);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = etTitle.getText().toString();
+                String description = etDescription.getText().toString();
+                String location = etLocation.getText().toString();
+
+                // Save event to database
+                DatabaseHelper dbHelper = new DatabaseHelper(AddEventActivity.this);
+                dbHelper.addEvent(title, description, location);
+
+                Intent intent = new Intent();
+                intent.putExtra("title", title);
+                intent.putExtra("description", description);
+                intent.putExtra("location", location);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
 
