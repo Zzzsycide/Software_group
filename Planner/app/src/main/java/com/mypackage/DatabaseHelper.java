5c96ffc07f8ae5e7a038154662207536b0fd141a
Zzzsycide <2133553608@qq.com>
2025-04-15 17:01:46 +0800

    Update DatabaseHelper.java

--- Planner/app/src/main/java/com/mypackage/DatabaseHelper.java
+++ Planner/app/src/main/java/com/mypackage/DatabaseHelper.java
@@ -1,1 +1,69 @@
+package com.mypackage;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "events.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_EVENTS = "events";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_TITLE = "title";
+    private static final String COLUMN_DESCRIPTION = "description";
+    private static final String COLUMN_LOCATION = "location";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_EVENTS_TABLE = "CREATE TABLE " + TABLE_EVENTS + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_TITLE + " TEXT,"
+                + COLUMN_DESCRIPTION + " TEXT,"
+                + COLUMN_LOCATION + " TEXT)";
+        db.execSQL(CREATE_EVENTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_EVENTS);
+        onCreate(db);
+    }
+
+    public void addEvent(String title, String description, String location) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_TITLE, title);
+        values.put(COLUMN_DESCRIPTION, description);
+        values.put(COLUMN_LOCATION, location);
+        db.insert(TABLE_EVENTS, null, values);
+        db.close();
+    }
+
+    public static List<Event> getAllEvents(Context context) {
+        List<Event> events = new ArrayList<>();
+        DatabaseHelper dbHelper = new DatabaseHelper(context);
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_EVENTS, null);
+        if (cursor.moveToFirst()) {
+            do {
+                Event event = new Event();
+                event.setTitle(cursor.getString(cursor.getColumnIndex(COLUMN_TITLE)));
+                event.setDescription(cursor.getString(cursor.getColumnIndex(COLUMN_DESCRIPTION)));
+                event.setLocation(cursor.getString(cursor.getColumnIndex(COLUMN_LOCATION)));
+                events.add(event);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return events;
+    }
+}
 
