9265972bff1572e622a5d0abf1fca3be5b6c2a38
Zzzsycide <2133553608@qq.com>
2025-04-19 10:55:45 +0800

    Update MapFragment.java

--- Planner/app/src/main/java/com/mypackage/MapFragment.java
+++ Planner/app/src/main/java/com/mypackage/MapFragment.java
@@ -1,1 +1,17 @@
 
+package com.yourpackage;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback
