cc7d73e6466af845b9a1e3eaca92695940f3395d
Zzzsycide <2133553608@qq.com>
2025-04-19 11:06:01 +0800

    Update EventAdapter.java

--- Planner/app/src/main/java/com/mypackage/EventAdapter.java
+++ Planner/app/src/main/java/com/mypackage/EventAdapter.java
@@ -6,16 +6,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
+import java.util.Collections;
 import java.util.List;
 
 public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHolder> {
     private Context context;
     private List<Event> events;
 
-    public EventAdapter(Context context) {
+    public EventAdapter(Context context, List<Event> events) {
         this.context = context;
-        this.events = DatabaseHelper.getAllEvents(context);
+        this.events = events;
     }
 
     @NonNull
@@ -48,5 +50,59 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHol
             tvLocation = itemView.findViewById(R.id.tvLocation);
         }
     }
-}
 
+    // Method to get the list of events
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    // Method to update the list of events
+    public void setEvents(List<Event> events) {
+        this.events = events;
+        notifyDataSetChanged();
+    }
+
+    // ItemTouchHelper callback
+    public ItemTouchHelper.Callback getItemTouchHelperCallback() {
+        return new ItemTouchHelper.Callback() {
+            @Override
+            public int getMovementFlags(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
+                // Enable drag and drop
+                int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+                return makeMovementFlags(dragFlags, 0);
+            }
+
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                // Get the positions of the items being moved
+                int fromPosition = viewHolder.getAdapterPosition();
+                int toPosition = target.getAdapterPosition();
+
+                // Swap the items in the list
+                Collections.swap(events, fromPosition, toPosition);
+
+                // Notify the adapter of the change
+                notifyItemMoved(fromPosition, toPosition);
+
+                // Save the updated list to the database
+                saveEventsToDatabase();
+
+                return true;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                // Not used in this example
+            }
+
+            private void saveEventsToDatabase() {
+                // Save the updated list to the database
+                DatabaseHelper dbHelper = new DatabaseHelper(context);
+                dbHelper.deleteAllEvents(); // Clear the existing events
+                for (Event event : events) {
+                    dbHelper.addEvent(event.getTitle(), event.getDescription(), event.getLocation());
+                }
+            }
+        };
+    }
+}
