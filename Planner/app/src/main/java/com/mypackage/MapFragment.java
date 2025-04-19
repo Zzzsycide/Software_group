a7bdbfcb565f461f1c1f5d4beed8458c70f782e3
Zzzsycide <2133553608@qq.com>
2025-04-19 10:56:17 +0800

    Update MapFragment.java

--- Planner/app/src/main/java/com/mypackage/MapFragment.java
+++ Planner/app/src/main/java/com/mypackage/MapFragment.java
@@ -1,4 +1,3 @@
-
 package com.yourpackage;
 
 import android.os.Bundle;
@@ -8,10 +7,60 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+    private GoogleMap googleMap;
+    private LatLng eventLocation;
+
+    public MapFragment() {
+        // Required empty public constructor
+    }
+
+    public static MapFragment newInstance(double latitude, double longitude) {
+        MapFragment fragment = new MapFragment();
+        Bundle args = new Bundle();
+        args.putDouble("latitude", latitude);
+        args.putDouble("longitude", longitude);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            double latitude = getArguments().getDouble("latitude");
+            double longitude = getArguments().getDouble("longitude");
+            eventLocation = new LatLng(latitude, longitude);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        if (eventLocation != null) {
+            googleMap.addMarker(new MarkerOptions().position(eventLocation).title("Event Location"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(eventLocation, 15));
+        }
+    }
+}
